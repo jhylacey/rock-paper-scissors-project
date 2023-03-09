76b6f217e1c470d7a22eae6e365f399131afe1af
jhylacey <jhylacey@proton.me>
2023-03-09 11:27:51 -0600

    Add play round functionality

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-let compChoice;
-let playerChoice;
-let choices = ["rock", "paper", "scissors"]
+let compChoice = getComputerChoice();
+let pChoice;
+
 
 // get computers choice of rock paper or scissors
 
 function getComputerChoice() {
-    compChoice = choices[Math.floor(choices.length * Math.random())];
+    let choices = ["rock", "paper", "scissors"]
+    return choices[Math.floor(choices.length * Math.random())];
 }
 
 
 
-//function calls
+// play a single round and return result
+function playRound(compChoice, pChoice) {
+    pChoice = prompt('Choose between rock, paper, or scissors!', "").toLowerCase();
 
-getComputerChoice()
+    if (pChoice === compChoice) {
+        result = `It's a draw. You both picked ${pChoice}!`;
 
+    } else if (
+        (pChoice === 'rock' && compChoice === 'paper') ||
+        (pChoice === 'paper' && compChoice === 'scissors') ||
+        (pChoice === 'scissors' && compChoice === 'rock')) {
+        result = `You lost! ${compChoice} beats ${pChoice}!`;
+
+    } else {
+        result = `You win! ${pChoice} beats ${compChoice}!`;
+    }
+    return result;
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let roundResults = playRound(compChoice, pChoice);
+        if (roundResults = `You lost! ${compChoice} beats ${pChoice}!`) {
+            console.log('You lost!')
+        } else console.log('test')
+    }
+}
+
+playGame()
