f703e407d9151c08d0234b8fced992d2766f7e36
jhylacey <jhylacey@proton.me>
2023-03-10 16:59:15 -0600

    Finish UI

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-// // let compChoice = getComputerChoice();
-// pChoice = prompt('Choose between rock, paper, or scissors!', "").toLowerCase();
 let compChoice;
 let pChoice;
+
+let playerScore = 0;
+let compScore = 0;
+let roundCount = 0;
+
 
 
 
@@ -14,62 +17,132 @@ function getComputerChoice() {
 
 
 // play a single round and return result
-function playRound(compChoice, pChoice) {
-    pChoice = prompt("Choose between rock, paper, or scissors", "");
+
+
+
+function playRoundRock(compChoice, pChoice) {
+    pChoice = "rock";
     compChoice = getComputerChoice();
 
     if (pChoice === compChoice) {
         result = "draw";
-        console.log(`It's a draw this round. You both picked ${pChoice}!`)
+        roundCount = roundCount + 1;
+
+        displayResults(`It's a draw this round. You both picked ${pChoice}!`)
 
     } else if (
         (pChoice === 'rock' && compChoice === 'paper') ||
         (pChoice === 'paper' && compChoice === 'scissors') ||
         (pChoice === 'scissors' && compChoice === 'rock')) {
         result = "loss";
-        console.log(`You lost a round! ${compChoice} beats ${pChoice}!`)
+        compScore = compScore + 1;
+
+        displayResults(`You lost a round! ${compChoice} beats ${pChoice}!`)
 
     } else {
         result = "win";
-        console.log(`You win a round! ${pChoice} beats ${compChoice}!`)
+        playerScore = playerScore + 1;
+        roundCount = roundCount + 1;
+        displayResults(`You win a round! ${pChoice} beats ${compChoice}!`)
     }
     return result;
 }
 
+function playRoundPaper(compChoice, pChoice) {
+    pChoice = "paper";
+    compChoice = getComputerChoice();
 
-// function playGame
-// playRound(compChoice, pChoice)
+    if (pChoice === compChoice) {
+        result = "draw";
+        displayResults(`It's a draw this round. You both picked ${pChoice}!`)
+
+    } else if (
+        (pChoice === 'rock' && compChoice === 'paper') ||
+        (pChoice === 'paper' && compChoice === 'scissors') ||
+        (pChoice === 'scissors' && compChoice === 'rock')) {
+        result = "loss";
+        compScore = compScore + 1;
+        roundCount = roundCount + 1;
 
 
-function playGame() {
-    let playerScore = 0;
-    let compScore = 0;
-    for (let i = 0; i < 5; i++) {
-        playRound(compChoice, pChoice);
-        if (result === "win") {
-            playerScore = playerScore + 1;
-        } else if (result === "loss") {
-            compScore = compScore + 1;
-        } else continue;
+        displayResults(`You lost a round! ${compChoice} beats ${pChoice}!`)
+
+    } else {
+        result = "win";
+        playerScore = playerScore + 1;
+        roundCount = roundCount + 1;
+        displayResults(`You win a round! ${pChoice} beats ${compChoice}!`)
     }
-
-    if (playerScore > compScore) {
-        console.log(`Final Score: ${playerScore} - ${compScore}! You win!`)
-    } else if (playerScore < compScore) {
-        console.log(`Final Score: ${playerScore} - ${compScore}! You lose!`)
-    } else console.log(`Final Score: ${playerScore} - ${compScore}! Tie game!`);
+    return result;
 }
-playGame()
+
+displayResults = (results) => {
+    document.querySelector('#gameResults').textContent = results
+}
+
+function playRoundScissors(compChoice, pChoice) {
+    pChoice = "scissors";
+    compChoice = getComputerChoice();
+
+    if (pChoice === compChoice) {
+        result = "draw";
+        roundCount++;
+
+        displayResults(`It's a draw this round. You both picked ${pChoice}!`)
+
+    } else if (
+        (pChoice === 'rock' && compChoice === 'paper') ||
+        (pChoice === 'paper' && compChoice === 'scissors') ||
+        (pChoice === 'scissors' && compChoice === 'rock')) {
+        result = "loss";
+        compScore = compScore + 1;
+        roundCount++;
+
+        displayResults(`You lost a round! ${compChoice} beats ${pChoice}!`)
+
+    } else {
+        result = "win";
+        playerScore = playerScore + 1;
+        roundCount++;
+        displayResults(`You win a round! ${pChoice} beats ${compChoice}!`)
+    }
+    return roundCount;
+
+}
 
 
+
+//select buttons
+const rockBtn = document.querySelector('#rockBtn');
+rockBtn.addEventListener("click", playRoundRock)
+
+const paperBtn = document.querySelector('#paperBtn');
+paperBtn.addEventListener("click", playRoundPaper)
+
+const scissorsBtn = document.querySelector('#scissorBtn');
+scissorsBtn.addEventListener("click", playRoundScissors)
+
+
+
+
+//play a game of 5 rounds
 
 // function playGame() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound(compChoice, pChoice);
-//     }
+//     let playerScore = 0;
+//     let compScore = 0;
+//     let roundCount = 0;
+
+//     if (playerScore > compScore) {
+//         console.log(`Final Score: ${playerScore} - ${compScore}! You win!`)
+//     } else if (playerScore < compScore) {
+//         console.log(`Final Score: ${playerScore} - ${compScore}! You lose!`)
+//     } else console.log(`Final Score: ${playerScore} - ${compScore}! Tie game!`);
 // }
 
 
+//function calls 
+
 // playGame()
 
-// // playRound()
+
+console.log(roundCount)
